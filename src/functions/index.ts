5b3a0a53039433449cdb038a724607738376e639
Oleg Dramaretskyi <dramarec@gmail.com>
2021-09-09 01:28:54 +0300

    update swagger

--- src/functions/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as getProductsById } from './getProductsById';
-export { default as getAllProducts } from './getProductsList';
--- src/types/index.ts
+++
@@ -1,9 +0,0 @@
-export default interface ICards {
-    id: string
-    name: string
-    description: string
-    image: string
-    price: number
-    favorite: boolean
-    ingredients: string[]
-}
